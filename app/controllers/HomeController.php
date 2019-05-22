0f07851b0d1aae0b9a8068d96e40f659fd494fda
Juan Quintanilla <quintanilla.peru@gmail.com>
2019-05-21 22:52:07 -0500

    ajustes finales de la extension

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,17 +3,24 @@
 class HomeController extends BaseController {
 
   public function getHome()
+  // carga el archivo de vista home 
+  // se envía la relación de procesos (uploads)
   {
     $proceso = Proceso::take(10)->orderBy('created_at', 'desc')->get();
     return View::make('home')->with('procesos', $proceso);
   }
 
-  public function postProceso() {
-    $fechaHoy = date('d/m/Y H:i:s');
+  public function postProceso()
+  // función para los uploads
+  // se recibe los datos por POST
+  {
+    // verificar si el archivo está presente
     if (!Input::hasFile('archivo')) {
       exit('No autorizado!');
     }
+    $fechaHoy = date('d/m/Y H:i:s');
     try {
+      // obtener el archivo para procesarlo
       $public_path = public_path();
       $carpeta = 'procesos';
       $extension = Input::file('archivo')->getClientOriginalExtension();
@@ -21,19 +28,28 @@ class HomeController extends BaseController {
       $archivo = Input::file('archivo')->move($carpeta, $archivo);
       $archivoRuta = $public_path . '/' . $archivo;
       $procesoDescripcion = Input::get('proceso', $fechaHoy);
+      // variable tipo array para almacenar la data del archivo y su posterior manejo en la base de datos
       $filaProceso = [];
-      // recorrer las filas de la hoja INGRESO
+      // leer el archivo y
+      // recorrer las filas de la hoja INGRESO, enviar el array para su uso
       Excel::selectSheets('INGRESO')->load($archivoRuta, function($reader) use(&$filaProceso) {
+        // flag para detectar presencia de datos/encabezados
         $hayDatos = false;
         $fila = 0;
         $contador = 1;
+        // desactivar la lectura automática de encabezados
         $reader->noHeading();
+        // obtener las filas
         $rows = $reader->get();
-        foreach($rows as $key => $row) { // recorrer las filas
+        // recorrer las filas
+        foreach($rows as $key => $row) {
           $primeraCelda = trim($row[0]);
           if ($primeraCelda != '' && $fila == 0) $hayDatos = true;
           if ($hayDatos) {
+            // escribir el array según la estructura de la hoja
             if ($contador == 1) {
+              // este grupo de datos representa
+              // la primera fila de datos
               $filaProceso[$fila]['igr_area'] = trim($row[1]);
               $filaProceso[$fila]['igr_ges'] = trim($row[2]);
               $filaProceso[$fila]['igr_trabajador'] = trim($row[5]);
@@ -42,6 +58,9 @@ class HomeController extends BaseController {
               $filaProceso[$fila]['igr_neqdbc'] = trim($row[21]);
               $filaProceso[$fila]['igr_peakc'] = trim($row[22]);
             } else {
+              // este grupo de datos representa
+              // las siguientes filas de datos y 
+              // se concatenan en el array con el caracter |
               if (trim($row[12]) != '')
                 $filaProceso[$fila]['igr_fri'] .= ' | ' . trim($row[12]);
               if (trim($row[19]) != '')
@@ -64,20 +83,30 @@ class HomeController extends BaseController {
         $cuenta = 0;
         $fila = 0;
         $contador = 1;
+        // desactivar la lectura automática de encabezados
         $reader->noHeading();
+        // obtener las filas
         $rows = $reader->get();
+        // cantidad de filas de la primera hoja para evitar filas adicionales
         $totalFilas = count($filaProceso);
-        foreach($rows as $key => $row) { // recorrer las filas
+        // recorrer las filas
+        foreach($rows as $key => $row) {
           $primeraCelda = trim($row[0]);
           if ($primeraCelda != '') {
             $cuenta++;
           }
           if ($cuenta >= 2) {
+            // escribir el array según la estructura de la hoja
             if ($contador == 1) {
+              // este grupo de datos representa
+              // la primera fila de datos
               $filaProceso[$fila]['cic_neqdbc'] = trim($row[4]);
               $filaProceso[$fila]['cic_tm'] = trim($row[5]);
               $filaProceso[$fila]['cic_tej'] = trim($row[6]);
             } else {
+              // este grupo de datos representa
+              // las siguientes filas de datos y 
+              // se concatenan en el array con el caracter |
               if (trim($row[4]) != '')
                 $filaProceso[$fila]['cic_neqdbc'] .= ' | ' . trim($row[4]);
               if (trim($row[5]) != '')
@@ -94,15 +123,19 @@ class HomeController extends BaseController {
           }
         }
       });
-      // grabar el proceso
+      // grabar el proceso en la base de datos
       $proceso = new Proceso;
       $proceso->descripcion = $procesoDescripcion;
       $proceso->archivo = $archivo;
       $proceso->estado = 1;
+      // almacenar el proceso
       $proceso->save();
+      // obtener el id de proceso
       $procesoId = $proceso->id;
       // grabar detalle de proceso
+      // recorrer el array $filaProceso
       foreach($filaProceso as $detalle) {
+        // armar la estructura para las filas del archivo
         $procesoDetalle = new ProcesoDetalle;
         $procesoDetalle->id_proceso = $procesoId;
         $procesoDetalle->igr_area = $detalle['igr_area'];
@@ -110,6 +143,9 @@ class HomeController extends BaseController {
         $procesoDetalle->igr_trabajador = $detalle['igr_trabajador'];
         $procesoDetalle->igr_fri = $detalle['igr_fri'];
         $procesoDetalle->igr_ciclott = $detalle['igr_ciclott'];
+        // a continuación los campos concatenados se separan con 'explode' y 
+        // se almacenan en los campos de base de datos
+        // siempre que esten presentes 'isset'
         $igr_neqdbc = explode('|', $detalle['igr_neqdbc']);
         $procesoDetalle->igr_neqdbc_1 = $igr_neqdbc[0];
         $procesoDetalle->igr_neqdbc_2 = isset($igr_neqdbc[1]) ? $igr_neqdbc[1] : 0;
@@ -140,12 +176,14 @@ class HomeController extends BaseController {
         $procesoDetalle->cic_tej_3 = isset($cic_tej[2]) ? $cic_tej[2] : 0;
         $procesoDetalle->cic_tej_4 = isset($cic_tej[3]) ? $cic_tej[3] : 0;
         $procesoDetalle->cic_tej_5 = isset($cic_tej[4]) ? $cic_tej[4] : 0;
+        // almacenar los registros de datos
         $procesoDetalle->save();
       }
-      // cargar vista con resultados: $filas por ejemplo
+      // cargar vista con resultados cantidad de filas procesadas
       $filas = count($filaProceso);
       return View::make('resultados')->with('filas', $filas)->with('proceso', $procesoDescripcion);
     } catch (Exception $e) {
+      // si sucedió un error devolver el mensaje de error
       $procesoDescripcion = Input::get('proceso', $fechaHoy);
       $mensaje = $e->getMessage();
       return View::make('error')->with('mensaje', $mensaje)->with('proceso', $procesoDescripcion);
@@ -153,11 +191,14 @@ class HomeController extends BaseController {
   }
 
   public function getHistorial() {
+    // obtener la lista de últimos 10 procesos
     $proceso = Proceso::take(10)->orderBy('created_at', 'desc')->get();
     return View::make('historial')->with('procesos', $proceso);
   }
 
   public function getHistorialById($idProceso) {
+    // obtenr las filas procesadas por proceso
+    // descarga el archivo con las filas
     $proceso = Proceso::find($idProceso);
     $archivo = $proceso->descripcion == '' ? 'archivo' : $proceso->descripcion;
     $procesoDetalle = ProcesoDetalle::where('id_proceso', $idProceso)->select('igr_area', 'igr_ges', 'igr_trabajador', 'igr_fri', 'igr_ciclott', 'igr_neqdbc_1', 'igr_neqdbc_2', 'igr_neqdbc_3', 'igr_neqdbc_4', 'igr_neqdbc_5', 'igr_peakc_1', 'igr_peakc_2', 'igr_peakc_3', 'igr_peakc_4', 'igr_peakc_5', 'cic_neqdbc_1', 'cic_neqdbc_2', 'cic_neqdbc_3', 'cic_neqdbc_4', 'cic_neqdbc_5', 'cic_tm_1', 'cic_tm_2', 'cic_tm_3', 'cic_tm_4', 'cic_tm_5', 'cic_tej_1', 'cic_tej_2', 'cic_tej_3', 'cic_tej_4', 'cic_tej_5')->get();
@@ -169,15 +210,109 @@ class HomeController extends BaseController {
   }
 
   public function getOnline() {
+    // obtiene los datos para armar el consolidado
     $procesoDetalle = ProcesoDetalle::orderBy('created_at', 'DESC')->get();
     return View::make('consolidado')->with('procesos', $procesoDetalle);
   }
 
   public function getConsolidado() {
+    // obtiene el consolidado en archivo excel para la descarga
     $archivo = 'consolidado_'.date('dMY').'.xlsx';
     $procesoDetalle = ProcesoDetalle::orderBy('created_at', 'DESC')->get();
-    Excel::create($archivo, function($excel) use($procesoDetalle) {
-      //
+    // variable tipo array para almacenar la data y escribir el archivo
+    $consolidado = [];
+    $fila = 0;
+    $registro = 1;
+    // primera fila con los encabezados de columnas
+    $consolidado[$fila][0] = '';
+    $consolidado[$fila][1] = 'ÁREA DEPARTAMENTO';
+    $consolidado[$fila][2] = 'GES Grupo de Exposición Similar';
+    $consolidado[$fila][3] = 'Trabajador';
+    $consolidado[$fila][4] = 'Fuentes de Ruido Incidentes (y estado)';
+    $consolidado[$fila][5] = 'Ciclo de Trabajo o Tareas incluidas en medición';
+    $consolidado[$fila][6] = 'Neq dBC';
+    $consolidado[$fila][7] = 'PeakC';
+    $consolidado[$fila][8] = 'Neq dBA Cada Ciclo';
+    $consolidado[$fila][9] = 'Tiempo Medición (minutos)';
+    $consolidado[$fila][10] = 'Tiempo Efectivo por jornada (horas)';
+    // recorrer los datos y armar la estructura del consolidado
+    foreach($procesoDetalle as $detalle) {
+      $fila++;
+      $consolidado[$fila][0] = $registro;
+      $consolidado[$fila][1] = $detalle['igr_area'];
+      $consolidado[$fila][2] = $detalle['igr_ges'];
+      $consolidado[$fila][3] = $detalle['igr_trabajador'];
+      $consolidado[$fila][4] = $detalle['igr_fri'];
+      $consolidado[$fila][5] = $detalle['igr_ciclott'];
+      $consolidado[$fila][6] = $detalle['igr_neqdbc_1'];
+      $consolidado[$fila][7] = $detalle['igr_peakc_1'];
+      $consolidado[$fila][8] = $detalle['cic_neqdbc_1'];
+      $consolidado[$fila][9] = $detalle['cic_tm_1'];
+      $consolidado[$fila][10] = $detalle['cic_tej_1'];
+      if ($detalle->igr_neqdbc_2 > 0 || $detalle->igr_peakc_2 > 0 || $detalle->cic_neqdbc_2 > 0 || $detalle->cic_tm_2 > 0 || $detalle->cic_tej_2 > 0) {
+        $fila++;
+        $consolidado[$fila][0] = '';
+        $consolidado[$fila][1] = '';
+        $consolidado[$fila][2] = '';
+        $consolidado[$fila][3] = '';
+        $consolidado[$fila][4] = '';
+        $consolidado[$fila][5] = '';
+        $consolidado[$fila][6] = $detalle['igr_neqdbc_2'];
+        $consolidado[$fila][7] = $detalle['igr_peakc_2'];
+        $consolidado[$fila][8] = $detalle['cic_neqdbc_2'];
+        $consolidado[$fila][9] = $detalle['cic_tm_2'];
+        $consolidado[$fila][10] = $detalle['cic_tej_2'];
+      }
+      if ($detalle->igr_neqdbc_3 > 0 || $detalle->igr_peakc_3 > 0 || $detalle->cic_neqdbc_3 > 0 || $detalle->cic_tm_3 > 0 || $detalle->cic_tej_3 > 0) {
+        $fila++;
+        $consolidado[$fila][0] = '';
+        $consolidado[$fila][1] = '';
+        $consolidado[$fila][2] = '';
+        $consolidado[$fila][3] = '';
+        $consolidado[$fila][4] = '';
+        $consolidado[$fila][5] = '';
+        $consolidado[$fila][6] = $detalle['igr_neqdbc_3'];
+        $consolidado[$fila][7] = $detalle['igr_peakc_3'];
+        $consolidado[$fila][8] = $detalle['cic_neqdbc_3'];
+        $consolidado[$fila][9] = $detalle['cic_tm_3'];
+        $consolidado[$fila][10] = $detalle['cic_tej_3'];
+      }
+      if ($detalle->igr_neqdbc_4 > 0 || $detalle->igr_peakc_4 > 0 || $detalle->cic_neqdbc_4 > 0 || $detalle->cic_tm_4 > 0 || $detalle->cic_tej_4 > 0) {
+        $fila++;
+        $consolidado[$fila][0] = '';
+        $consolidado[$fila][1] = '';
+        $consolidado[$fila][2] = '';
+        $consolidado[$fila][3] = '';
+        $consolidado[$fila][4] = '';
+        $consolidado[$fila][5] = '';
+        $consolidado[$fila][6] = $detalle['igr_neqdbc_4'];
+        $consolidado[$fila][7] = $detalle['igr_peakc_4'];
+        $consolidado[$fila][8] = $detalle['cic_neqdbc_4'];
+        $consolidado[$fila][9] = $detalle['cic_tm_4'];
+        $consolidado[$fila][10] = $detalle['cic_tej_4'];
+      }
+      if ($detalle->igr_neqdbc_5 > 0 || $detalle->igr_peakc_5 > 0 || $detalle->cic_neqdbc_5 > 0 || $detalle->cic_tm_5 > 0 || $detalle->cic_tej_5 > 0) {
+        $fila++;
+        $consolidado[$fila][0] = '';
+        $consolidado[$fila][1] = '';
+        $consolidado[$fila][2] = '';
+        $consolidado[$fila][3] = '';
+        $consolidado[$fila][4] = '';
+        $consolidado[$fila][5] = '';
+        $consolidado[$fila][6] = $detalle['igr_neqdbc_5'];
+        $consolidado[$fila][7] = $detalle['igr_peakc_5'];
+        $consolidado[$fila][8] = $detalle['cic_neqdbc_5'];
+        $consolidado[$fila][9] = $detalle['cic_tm_5'];
+        $consolidado[$fila][10] = $detalle['cic_tej_5'];
+      }
+      $registro++;
+    }
+    // declarar el archivo y descargar
+    Excel::create($archivo, function($excel) use($consolidado) {
+      $excel->sheet('Consolidado', function($sheet) use($consolidado) {
+        $sheet->fromArray($consolidado, null, 'A1', false, false);
+        $sheet->freezeFirstRow();
+      });
     })->download('xlsx');
   }
 
--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,19 @@
 <?php
 
+// archivo de rutas HTTP
+
+// página de inicio, se muestra el formulario de upload e historial
 Route::get('/', array('uses' => 'HomeController@getHome'));
+// ver el historial de uploads
 Route::get('historial', array('uses' => 'HomeController@getHistorial'));
+// ver el detalle del upload
 Route::get('getHistorial/{idProceso}', array('uses' => 'HomeController@getHistorialById'));
+// ver el consolidado
 Route::get('consolidado', array('uses' => 'HomeController@getOnline'));
+// descargar el consolidado
 Route::get('descargarConsolidado', array('uses' => 'HomeController@getConsolidado'));
 
-//Route::post('proceso', array('before' => 'csrf', function()
+// ruta para el upload
 Route::group(array('before' => 'csrf'), function()
 {
   Route::post('proceso', array('uses' => 'HomeController@postProceso'));
