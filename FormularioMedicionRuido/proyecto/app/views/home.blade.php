c0b220841a43b27a49ef7b188fe1608024c03c86
Juan Quintanilla <quintanilla.peru@gmail.com>
2019-05-23 08:36:13 -0500

    ajustes

--- FormularioMedicionRuido/proyecto/app/views/home.blade.php
+++ FormularioMedicionRuido/proyecto/app/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="card hoverable">
 	<div class="card-content">
-		<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget tincidunt libero. Curabitur scelerisque ante ultricies posuere accumsan. Donec at efficitur nisl, non posuere ante. Nam vitae malesuada justo.</p>
 	</div>
 	<div class="card-tabs">
 		<ul class="tabs tabs-fixed-width">
@@ -13,20 +13,321 @@
 	</div>
 	<div class="card-content grey lighten-4">
 		<div id="paso1">
-			<div class="card blue-grey darken-1">
-				<div class="card-content white-text">
-					<span class="card-title">FICHA DE INFORMACIÓN DE MEDICIÓN DE RUIDO</span>
-					<p>IDENTIFICACIÓN DE LA FUENTE EMISORA DE RUIDO</p>
+			<div class="card blue lighten-5">
+				<div class="card-content">
+					<p class="card-title">FICHA DE INFORMACIÓN DE MEDICIÓN DE RUIDO</p>
 					<form>
+						<p><i class="tiny material-icons">send</i> IDENTIFICACIÓN DE LA FUENTE EMISORA DE RUIDO</p>
 						<div class="row">
-							<div class="input-field col s6">
+							<div class="input-field col s12 m6">
 								<input id="nombre" name="nombre" type="text" class="validate">
 								<label for="nombre">Nombre o razón social</label>
 							</div>
-							<div class="input-field col s6">
+							<div class="input-field col s12 m6">
 								<input id="rut" name="rut" type="text" class="validate">
 								<label for="rut">RUT</label>
 							</div>
+							<div class="input-field col s12 m6">
+								<input id="direccion" name="direccion" type="text" class="validate">
+								<label for="direccion">Dirección</label>
+							</div>
+							<div class="input-field col s12 m6">
+								<input id="comuna" name="comuna" type="text" class="validate">
+								<label for="comuna">Comuna</label>
+							</div>
+							<div class="input-field col s12">
+								<input id="zona" name="zona" type="text" class="validate">
+								<label for="zona">Nombre de Zona de emplazamiento (según IPT vigente)</label>
+							</div>
+							<div class="input-field col s12 m6">
+								<input id="datum" name="datum" type="text" class="validate">
+								<label for="datum">Datum</label>
+							</div>
+							<div class="input-field col s12 m6">
+								<input id="huso" name="huso" type="text" class="validate">
+								<label for="huso">Huso</label>
+							</div>
+							<div class="input-field col s12 m6">
+								<input id="coordenada-norte" name="coordenada-norte" type="text" class="validate">
+								<label for="coordenada-norte">Coordenada Norte</label>
+							</div>
+							<div class="input-field col s12 m6">
+								<input id="coordenada-este" name="coordenada-este" type="text" class="validate">
+								<label for="coordenada-este">Coordenada Este</label>
+							</div>
+						</div>
+						<p><i class="tiny material-icons">send</i> CARACTERIZACIÓN DE LA FUENTE EMISORA DE RUIDO</p>
+						<div class="row">
+							<div class="grey lighten-1 white-text col s12 l5 xl4">
+								Actividad Productiva
+							</div>
+							<div class="col s12 l7 xl8">
+								<div class="row">
+									<div class="col s12 m6 l3">
+										<label>
+											<input type="checkbox" name="actividad-productiva" value="industrial" />
+											<span>Industrial</span>
+										</label>
+									</div>
+									<div class="col s12 m6 l3">
+										<label>
+											<input type="checkbox" name="actividad-productiva" value="agricola" />
+											<span>Agrícola</span>
+										</label>
+									</div>
+									<div class="col s12 m6 l3">
+										<label>
+											<input type="checkbox" name="actividad-productiva" value="extraccion" />
+											<span>Extracción</span>
+										</label>
+									</div>
+									<div class="col s12 m6 l3">
+										<label>
+											<input type="checkbox" name="actividad-productiva" value="otro" />
+											<span>Otro</span>
+										</label>
+									</div>
+								</div>
+						</div>
+						<div class="row flex">
+							<div class="grey lighten-1 white-text col s12 l5 xl4">
+								Actividad Comercial
+							</div>
+							<div class="col s12 l7 xl8">
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="restaurant" />
+										<span>Restaurant</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="taller-mecanico" />
+										<span>Taller Mecánico</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="local-comercial" />
+										<span>Local Comercial</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="otro" />
+										<span>Otro</span>
+									</label>
+								</div>
+						</div>
+						<div class="row flex">
+							<div class="grey lighten-1 white-text col s12 l5 xl4">
+								Actividad Esparcimiento
+							</div>
+							<div class="col s12 l7 xl8">
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-esparcimiento" value="discoteca" />
+										<span>Discoteca</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-esparcimiento" value="recinto-deportivo" />
+										<span>Recinto Deportivo</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-esparcimiento" value="cultura" />
+										<span>Cultura</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-esparcimiento" value="otro" />
+										<span>Otro</span>
+									</label>
+								</div>
+						</div>
+						<div class="row flex">
+							<div class="grey lighten-1 white-text col s12 l5 xl4">
+								Actividad Comercial
+							</div>
+							<div class="col s12 l7 xl8">
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="restaurant" />
+										<span>Restaurant</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="taller-mecanico" />
+										<span>Taller Mecánico</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="local-comercial" />
+										<span>Local Comercial</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="otro" />
+										<span>Otro</span>
+									</label>
+								</div>
+						</div>
+						<div class="row flex">
+							<div class="grey lighten-1 white-text col s12 l5 xl4">
+								Actividad Comercial
+							</div>
+							<div class="col s12 l7 xl8">
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="restaurant" />
+										<span>Restaurant</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="taller-mecanico" />
+										<span>Taller Mecánico</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="local-comercial" />
+										<span>Local Comercial</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="otro" />
+										<span>Otro</span>
+									</label>
+								</div>
+						</div>
+						<div class="row flex">
+							<div class="grey lighten-1 white-text col s12 l5 xl4">
+								Actividad Comercial
+							</div>
+							<div class="col s12 l7 xl8">
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="restaurant" />
+										<span>Restaurant</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="taller-mecanico" />
+										<span>Taller Mecánico</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="local-comercial" />
+										<span>Local Comercial</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="otro" />
+										<span>Otro</span>
+									</label>
+								</div>
+						</div>
+						<div class="row flex">
+							<div class="grey lighten-1 white-text col s12 l5 xl4">
+								Actividad Comercial
+							</div>
+							<div class="col s12 l7 xl8">
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="restaurant" />
+										<span>Restaurant</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="taller-mecanico" />
+										<span>Taller Mecánico</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="local-comercial" />
+										<span>Local Comercial</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="otro" />
+										<span>Otro</span>
+									</label>
+								</div>
+						</div>
+						<div class="row flex">
+							<div class="grey lighten-1 white-text col s12 l5 xl4">
+								Actividad Comercial
+							</div>
+							<div class="col s12 l7 xl8">
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="restaurant" />
+										<span>Restaurant</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="taller-mecanico" />
+										<span>Taller Mecánico</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="local-comercial" />
+										<span>Local Comercial</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="otro" />
+										<span>Otro</span>
+									</label>
+								</div>
+						</div>
+						<div class="row flex">
+							<div class="grey lighten-1 white-text col s12 l5 xl4">
+								Actividad Comercial
+							</div>
+							<div class="col s12 l7 xl8">
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="restaurant" />
+										<span>Restaurant</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="taller-mecanico" />
+										<span>Taller Mecánico</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="local-comercial" />
+										<span>Local Comercial</span>
+									</label>
+								</div>
+								<div class="col s12 m6 l3">
+									<label>
+										<input type="checkbox" name="actividad-comercial" value="otro" />
+										<span>Otro</span>
+									</label>
+								</div>
 						</div>
 				</div>
 				<div class="card-action">
